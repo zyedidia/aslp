96a5d7713bdb9f7da01b34415f8fd47f9f44a29d
Docker <docker@example.com>
2022-09-15 02:56:17 +0100

    Added shell scripts to perform bap related tasks

---
+++ scripts/package_asli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Requires that you have created the asli package with `opam pin add . -k path`, 
+# which will have been done if you are using the dockerfile.
+# If you are having issues with your changes not getting included (opam says 'no changes'),
+# try unpinning and repinning the package.
+
+cd $HOME/asl-interpreter &&
+dune build &&
+opam install asli
