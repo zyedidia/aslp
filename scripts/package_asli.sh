f9642b32c4d3abb1a7874e4d07db94b0f0759b62
JamesP797 <jamespaterson@outlook.com.au>
2022-11-14 04:22:24 +0000

    Added some more information to the package asli script

--- scripts/package_asli.sh
+++ scripts/package_asli.sh
@@ -2,7 +2,9 @@
 # Requires that you have created the asli package with `opam pin add . -k path`, 
 # which will have been done if you are using the dockerfile.
 # If you are having issues with your changes not getting included (opam says 'no changes'),
-# try unpinning and repinning the package.
+# try unpinning and repinning the package:
+#   opam pin remove .
+#   opam pin add . -k path
 
 cd $HOME/asl-interpreter && eval $(opam env) &&
 dune build &&
