4ff8abc8d3afadbda46f37cf444adfbad7b5b3e7
Alistair Michael <alistair.michael@uq.edu.au>
2024-02-08 17:42:17 +1000

    use http

--- bin/server.ml
+++ bin/server.ml
@@ -1,5 +1,10 @@
 open LibASL
 
+
+open Yojson 
+open Cohttp
+open Cohttp_lwt
+open Cohttp_lwt_unix
 open String
 open List
 open Array 
@@ -9,23 +14,8 @@ open Eval
 open Asl_utils
 open Lwt
 
-let file_load_order =  ["mra_tools/arch/regs.asl"; "mra_tools/types.asl"; "mra_tools/arch/arch.asl"; "mra_tools/arch/arch_instrs.asl"; 
-        "mra_tools/arch/arch_decode.asl"; "mra_tools/support/aes.asl";"mra_tools/support/barriers.asl";"mra_tools/support/debug.asl"; 
-        "mra_tools/support/feature.asl"; "mra_tools/support/hints.asl";"mra_tools/support/interrupts.asl"; "mra_tools/support/memory.asl"; 
-        "mra_tools/support/stubs.asl"; "mra_tools/support/fetchdecode.asl"; "tests/override.asl" ]
 
-
-let default_asl_files : string = match (Res.Sites.aslfiles) with 
-    | hd :: _ -> hd
-    | _ -> failwith "unable to load bundled asl files."
-
-let envinfo : Asl_ast.declaration list =
-  let prelude : Asl_ast.declaration list = LoadASL.read_file (Filename.concat default_asl_files  "prelude.asl") true false     in
-  let mra : Asl_ast.declaration list  list   = List.map (fun t -> LoadASL.read_file (Filename.concat default_asl_files t)  false false) file_load_order in
-  List.concat (prelude :: mra)
-
-
-let persistent_env     = Eval.build_evaluation_environment envinfo
+let persistent_env     = Option.get (aarch64_evaluation_environment ()) 
 
 let eval_instr (opcode: string) : string = 
     let praw a : string = Utils.to_string (Asl_parser_pp.pp_raw_stmt a) |> String.trim  in
@@ -36,46 +26,53 @@ let eval_instr (opcode: string) : string =
     List.map indiv ascii |>  List.map (String.concat "") |> String.concat "\n"
 
 
+let get_reply (jsonin: string) : string = 
+  (*let json  = Yojson.Safe.from_string jsonin in *)
+   match (eval_instr jsonin) with 
+  | exception e ->  Yojson.Safe.to_string  (`Assoc [("instruction", `String jsonin); ("error", `String (Printexc.to_string e))])
+  | x -> Yojson.Safe.to_string (`Assoc [("instruction", `String jsonin); ("semantics", `String x)] )
+  | _ ->  Yojson.Safe.to_string  (`Assoc [("instruction", `String jsonin); ("error", `String "unknown")])
+
+
+let unsupp_method_resp : string = (Yojson.Safe.to_string  (`Assoc [("error", `String "unsupported method.")]))
+let missing_param : string = (Yojson.Safe.to_string  (`Assoc [("error", `String "missing opcode param.")]))
+
+(*let () = ignore (List.map (fun (f: string) -> print_endline (eval_instr f)) (tl (to_list Sys.argv))) *)
+
+
 let get_resp (opcode: string) : string = 
-    match eval_instr opcode with 
-        | exception e -> Printexc.to_string e
-        | res -> res
+    get_reply opcode
 
-let rec handle_connection ic oc () =
-    Lwt_io.read_line_opt ic >>=
-    (fun msg ->
-        match msg with
-        | Some msg -> 
-            let reply = get_resp msg in
-            Lwt_io.write_line oc reply >>= handle_connection ic oc
-        | None -> return (print_endline "closed" ))
+let server addr port =
+  let callback _conn req body =
+    let uri = req |> Request.uri in
+    let meth = req |> Request.meth |> Code.string_of_method in
+    let headers = req |> Request.headers |> Header.to_string in
+    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
+        match  (Request.meth req) with 
+            | `POST -> get_resp body
+            | `GET ->  (match  (Uri.get_query_param uri "opcode") with 
+                | Some s ->  get_resp s
+                | None -> missing_param)
+            |  _ ->  unsupp_method_resp
+        )
+    >>= fun body -> Server.respond_string ~status:`OK ~body ()
+  in
+  Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
 
-let create_socket (addr: string) : Lwt_unix.file_descr Lwt.t =
-    let backlog = 1 in
-    let open Lwt_unix in
-    let sock = socket PF_UNIX SOCK_STREAM 0 in
-    ((Lwt_unix.bind sock (ADDR_UNIX addr) >>= (fun f -> return (listen sock backlog) ))) >>= (fun () -> return sock)
+let port_opt : int ref = ref 8000
+let addr_opt : string ref = ref "127.0.0.1"
 
-let accept_connection conn  =
-    let fd, _ = conn in
-    let ic = Lwt_io.of_fd Lwt_io.Input fd in
-    let oc = Lwt_io.of_fd Lwt_io.Output fd in
-    Lwt.on_failure (handle_connection ic oc ()) (fun e ->  (print_endline (Printexc.to_string e)));
-    (fun f -> Lwt_io.printlf "New connection") |> async; 
-    return ()
+let speclist =
+  [
+    ("--host", Arg.Set_string addr_opt, "Server address (default loopback)");
+    ("--port", Arg.Set_int port_opt, "Server port (default 8000)");
+  ]
 
+let () = Arg.parse speclist ignore "server --host localhost --port 8000"
 
-let create_server sock =
-    print_endline "starting server" ;
-    let rec serve () = 
-        sock >>= Lwt_unix.accept >>= accept_connection >>= serve in
-    serve
 
 let () = 
-    let sockname = "/tmp/beans" in
-    let sock = (create_socket sockname) in
-    let delete_sockfile = sock >|= (fun s -> ((
-        Lwt_unix.on_signal Sys.sigint (fun _ -> (print_endline "goodbye"; Lwt_unix.unlink sockname; failwith "goodbye" |> ignore))
-        ) |> ignore)) in
-    let serve = (create_server sock) in 
-    Lwt_main.run @@ (delete_sockfile >>= (serve))
+     let address = Unix.(ADDR_INET (inet_addr_of_string !addr_opt, !port_opt)) in
+    ignore (Lwt_main.run (server !addr_opt !port_opt))
+
